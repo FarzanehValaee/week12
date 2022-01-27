ccc9cf063323eb97d6e94c873c6ab0d13998bb35
farzaneh <farzaneh.valaee79@gmail.com>
2022-01-27 03:54:12 -0800

    final

--- form.js
+++ form.js
@@ -1,54 +1,30 @@
-
 function handleSubmit(e){
-          e.preventDefult();
+          e.preventDefault();
           const formElement= e.target;
           const user_information ={}
           for(input of formElement){
                     if(input.name==="gender"){
                               user_information[input.name]=document.querySelector('input[name="gender"]:checked').value;       
                     }else{
-                              user_information[input.name] = input.value;
+                              user_information[input.id] = input.value;
                     }
           }
           console.log(user_information)
-}
-
-
-
-
-// document.getElementById("myForm").reset();
-// function addUser(){
-          
-// }
-// const form  = document.getElementById('myForm');
-// form.addEventListener('submit', (event) => {
-//     // handle the form data
-//     event.preventDefault()
-//     var radios = document.getElementsByName('gender');
-//     let currentGender
-//     let userNumber=0
-//     let user_information=[]
-//     form.submit();
-//     const fname = form.elements['exampleInputFirstName1'];
-//     const lname = form.elements['exampleInputLastName1'];
-//     const email = form.elements['exampleInputEmail1'];
-// //     const password = form.elements['exampleInputPassword1'];
-//     const nationality = form.elements['exampleFormControlSelect1']
-//     for (var i = 0, length = radios.length; i < length; i++) {
-//           if (radios[i].checked) {
-//            currentGender = radios[i].value;
-//           }
-//     }
-
-//     user_information.push(fname.value + lname.value);
-//     user_information.push(email.value);
-//     user_information.push(nationality.value);
-//     user_information.push(currentGender);
-//     userNumber++;
-//     console.log(user_information) 
-// });
-
-
-
-
-
+          document.getElementById("table").style.display="block";
+          let new_cell       
+          var table = document.getElementById("table");
+          var row = table.insertRow(-1);
+          for(let index in user_information){
+              new_cell=row.insertCell(index);
+              new_cell.innerHTML=user_information[index];
+              new_cell.style.padding="1px";
+              new_cell.style.border="1px solid black";
+      
+          }
+          return false;
+      }
+      
+      
+      
+      
+      
